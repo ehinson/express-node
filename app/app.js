731cb8f42d2534a597587bc46ad19411a3d87cd7
Erin Hinson <erin.hinson@XCELC02Q33V.local>
2016-12-24 17:40:03 -0500

    styling and menu

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const reload = require('reload');
 var app = express();
 var dataFile = require('./data/data.json');
 
-app.set('port', process.env.PORT  || 3000);
+app.set('port', process.env.PORT  || 4000);
 app.set('appData', dataFile);
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
