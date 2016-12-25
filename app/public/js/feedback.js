1ebc5e3f9e947d2228fbc76d6f7b7c708191b5a9
Erin Hinson <hinson.erin@gmail.com>
2016-12-25 02:15:13 -0700

    handle DELETE and re-do feedback.json

--- app/public/js/feedback.js
+++ app/public/js/feedback.js
@@ -1,15 +1,34 @@
 $(function() {
   $.getJSON('api', updateFeedback);
 
-  var output = '';
-  function updateFeedback(data) {
+  $('.nodex-form').submit(function(e) {
+    e.preventDefault();
+    $.post('api', {
+      name: $('#nodex-form-name').val(),
+      email: $('#nodex-form-email').val(),
+      comment: $('#nodex-form-comment').val()
+    }, updateFeedback);
+  });
 
+  $('.feedback-messages').on('click', function (e) {
+    if(e.target.className == 'delete'){
+      console.log("delete");
+      $.ajax({
+        url: 'api/' + e.target.id,
+        type: 'DELETE',
+        success: updateFeedback
+      })
+    }
+  })
+
+
+  function updateFeedback(data) {
+    var output = '';
     $.each(data, function(key, item) {
-        if (typeof(item) == 'object'){
-          updateFeedback(item);
-        }else {
-          output += item + '<br>';
-        }
+        output += '<h4>' + item.name + '</h4>';
+        output += '<p>' + item.email + '</p>';
+        output += '<p>' + item.message + '</p>';
+        output += '<p> <button class="delete" id="delete-comment-'+ key+ '">delete</button></p>';
     });
 
     $('.feedback-messages').html(output);
--- app/routes/api.js
+++ app/routes/api.js
@@ -1,5 +1,7 @@
 const express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var fs = require('fs');
 var feedbackData = require('../data/feedback.json');
 
 // express deprecated res.json(status, obj): Use res.status(status).json(obj) instead app/routes/api.js:6:7
@@ -7,5 +9,30 @@ var feedbackData = require('../data/feedback.json');
 router.get('/api', function (req, res) {
   res.status(200).json(feedbackData);
 });
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended:false}));
+
+router.post('/api', function (req, res) {
+  feedbackData.unshift(req.body);
+  fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf-8', function (err) {
+    if(err){
+      console.log(err);
+    }
+  });
+  res.status(200).json(feedbackData);
+
+});
+
+router.delete('/api/:id', function (req, res) {
+  feedbackData.splice(req.params.id, 1);
+  fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf-8', function (err) {
+    if(err){
+      console.log(err);
+    }
+  });
+  res.status(200).json(feedbackData);
+
+})
 
 module.exports  = router;
